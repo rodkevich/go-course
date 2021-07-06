a98d4cae634b80d970ba775da4bf775a01466d37
404 <ivan@test.it>
2021-07-06 16:59:50 +0300

    add: cleanup

---
+++ cmd/hw001/solution-hw-001.go
@@ -0,0 +1,20 @@
+/*
+Lesson_01:
+Create new application
+Use emoji package "github.com/kyokomi/emoji" to say `Hello, world :relaxed:` with emoji
+Run it
+*/
+
+package main
+
+import (
+	"fmt"
+	"github.com/kyokomi/emoji"
+)
+
+func main() {
+	fmt.Println(emoji.Sprint(`Hello, world :relaxed:`))
+}
+
+// Result:
+// Hello, world ☺️
