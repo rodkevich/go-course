f5504f6697c1400cb2f7648d2059f854eaeba59d
404 <ivan@test.it>
2021-07-13 02:30:20 +0300

    ref: some fixes after lint

--- homework/hw002/fibo/fibo.go
+++ homework/hw002/fibo/fibo.go
@@ -8,10 +8,10 @@ import (
 
 var (
 	//error to try customizing
-	errorLimitOf64bitSeq = errors.New("\nAbort app: wrong sequence size provided (max 64bit sequence size is 93)")
-	errorNegativeNumber  = errors.New("\nAbort app: negatives are not allowed")
-	errorBadEnvVars      = errors.New("\nAbort app: required env vars were not correct")
-	errorNotImplemented  = errors.New("\nAbort app: implementation is not there yet. See ya!")
+	errorLimitOf64bitSeq = errors.New("abort app: wrong sequence size provided. Max 64bit sequence size is 93")
+	errorNegativeNumber  = errors.New("abort app: negatives are not allowed")
+	errorBadEnvVars      = errors.New("abort app: required env vars were not correct")
+	errorNotImplemented  = errors.New("abort app: implementation is not there yet. See ya")
 	knownPrinters        = []struct {
 		name        string
 		implemented bool
@@ -87,9 +87,9 @@ func sizedSequence64bit(size int64) ([]int64, error) {
 	}
 	var (
 		counted int64
-		a       int64   = 0
-		b       int64   = 1
-		rtn     []int64 = []int64{a, b} // init with first two
+		a       int64
+		b       int64 = 1
+		rtn           = []int64{a, b} // init with first two
 	)
 	for i := int64(2); i <= size; i++ {
 		counted = a + b
@@ -144,15 +144,14 @@ func sizedSequence64bitWithCache(size int64, cache *Cache) ([]int64, error) {
 		}
 		*cache = out
 		return out, err
-	} else {
-		fmt.Println("\n* [Using cache]")
-		// some experiments to play
-		rtn := make([]int64, 0)
-		for _, v := range *cache {
-			rtn = append(rtn, v)
-		}
-		return rtn[0:size], nil
 	}
+	fmt.Println("\n* [Using cache]")
+	// some experiments to play
+	rtn := make([]int64, 0)
+	for _, v := range *cache {
+		rtn = append(rtn, v)
+	}
+	return rtn[0:size], nil
 }
 
 //printSlice is used for printing slices with various options
