4f19cac4b73058f7723b883f860d3167fc38adcc
404 <ivan@test.it>
2021-07-14 19:14:01 +0300

    upd:  log.Output & fmt

--- cmd/hw002/fibo/fibo.go
+++ cmd/hw002/fibo/fibo.go
@@ -3,12 +3,19 @@ package fibo
 import (
 	"errors"
 	"fmt"
+	"log"
+)
+
+var (
+	errWrongArg      = "error: wrong arg passed"
+	err64bitOverflow = "error: 64 bit overflowed : rtn size maximum equals 93"
 )
 
 // SizedSequence64bit returns Fibonacci array of required length
 func SizedSequence64bit(size int64) ([]int64, error) {
 	if size <= 0 {
-		return nil, errors.New("error: wrong arg passed")
+		_ = log.Output(1, "fibo: wrong input")
+		return nil, errors.New(errWrongArg)
 	}
 	var (
 		counted int64
@@ -17,12 +24,10 @@ func SizedSequence64bit(size int64) ([]int64, error) {
 		rtn           = []int64{a, b} // init with first two
 	)
 	for i := int64(2); i <= size; i++ {
-
 		counted = a + b
 		if counted < 0 {
-			fmt.Printf("! Warn: 64 bit overflow on %d iteration \n", i+1)
-			size = i
-			return rtn[0:size], errors.New("error: 64 bit overflowed : rtn size is now on maximum and equals 93")
+			_ = log.Output(1, "fibo: error: 64 bit overflow")
+			return nil, errors.New(err64bitOverflow)
 		}
 		// prepare vars for new iteration
 		a, b = b, counted
