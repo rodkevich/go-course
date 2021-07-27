44aab0946b884508d3ad66254dea275dcb934fd0
404 <ivan@test.it>
2021-07-27 05:21:48 +0300

    add: solution task3

--- cmd/hw006/task3/main.go
+++ cmd/hw006/task3/main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"github.com/rodkevich/go-course/homework/hw006/task03"
-	"log"
-	"os/exec"
 )
 
 func main() {
 	s := task03.NewWebServer()
-	go s.Run()
-	// check with use of curl
-	args := []string{
-		"127.0.0.1:5050",
-		"127.0.0.1:5050/call404",
-	}
-	for _, arg := range args {
-
-		out, err := exec.Command("curl","-X", "POST", arg).Output()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Response:\n %s", out)
-	}
+	print("127.0.0.1:5050")
+	s.Run()
 }
+
+	// go s.Run()
+	// // check with use of curl
+	// args := []string{
+	// 	"127.0.0.1:5050",
+	// 	"127.0.0.1:5050/call404",
+	// }
+	// for _, arg := range args {
+	//
+	// 	out, err := exec.Command("curl","-X", "POST", arg).Output()
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Printf("Response:\n %s", out)
+	// }
--- homework/hw006/task03/server.go
+++ homework/hw006/task03/server.go
@@ -2,13 +2,17 @@ package task03
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-const address = "127.0.0.1:5050"
+const (
+	address   = "127.0.0.1:5050"
+	indexPath = "./static/index.html"
+)
 
 type webServer struct {
 	Address string
@@ -27,6 +31,7 @@ func (s webServer) Run() {
 	r.HandleFunc("/", s.handler).Methods("GET", "POST")
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(s.Address, nil))
+	// log.Fatal(http.ListenAndServe(":5050", http.FileServer(http.Dir("./static"))))
 }
 
 func (s webServer) handler(w http.ResponseWriter, r *http.Request) {
@@ -36,13 +41,26 @@ func (s webServer) handler(w http.ResponseWriter, r *http.Request) {
 		s.ifGet(w, r)
 	case "POST":
 		s.ifPost(w, r)
+	default:
+		s.output(w, "Use GET or POST")
 	}
 }
 
 func (s webServer) ifPost(w http.ResponseWriter, r *http.Request) {
-	s.output(w, "Result:", "POST ...")
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	name := r.FormValue("name")
+	adr := r.FormValue("address")
+	hC := http.Cookie{
+		Name:  "token",
+		Value: name + ":" + adr,
+	}
+	http.SetCookie(w, &hC)
+	http.ServeFile(w, r, indexPath)
 }
 
 func (s webServer) ifGet(w http.ResponseWriter, r *http.Request) {
-	s.output(w, "Result:", "GET ...")
+	http.ServeFile(w, r, indexPath)
 }
