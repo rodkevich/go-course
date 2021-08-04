5085cf80b10e44e76a8f7d783cdddfe0372dd3ca
404 <ivan@test.it>
2021-08-05 01:56:09 +0300

    feat: 008 solution

--- cmd/hw008/app/main.go
+++ cmd/hw008/app/main.go
@@ -1,40 +1,47 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
-var DB = make(map[string]interface{})
+var (
+	port = os.Getenv("SAVETOKENSERVICEPORT")
+	DB   = make(map[string]Entry)
+)
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
+// Entry to save to fake-db
+type Entry struct {
+	Name      string    `json:"token"`
+	CreatedAt time.Time `json:"createdAt"`
+	ExpiredAt time.Time `json:"expiredAt"`
+}
+
+func setupRouter() (engine *gin.Engine) {
+	engine = gin.Default()
+
+	engine.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "i'm oline don't worry")
 	})
-	r.POST("/save-token", func(c *gin.Context) {
-		// token, _ := c.Params.Get("token")
-		type Token struct {
-			Name    string `json:"name" binding:"required"`
-			Address string `json:"address" binding:"required"`
-		}
-		var token Token
-		err := c.Bind(&token)
+
+	engine.POST("/save-token", func(c *gin.Context) {
+		var entry Entry
+		err := c.Bind(&entry)
 		if err != nil {
-			c.JSON(400, gin.H{"status": "validation_error", "error": err})
+			c.JSON(400, gin.H{"status": "validation_error", "error": err.Error()})
 			return
 		}
-
-		DB[token.Name] = token.Address
-		c.JSON(http.StatusOK, gin.H{"status": "ok", "token": string(token.Address)})
+		DB[entry.Name] = entry
+		c.JSON(http.StatusOK, gin.H{"status": "ok", "entry": &entry})
 	})
-	return r
+	return
 }
 
 func main() {
 	r := setupRouter()
-	r.Run(":5051")
+	r.Run(":" + port)
 }
